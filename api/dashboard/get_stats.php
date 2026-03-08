6a6707904e0f79ae12893925631f1ff83f36e56f
Clark Ken <clarkken.moron@cvsu.edu.ph>
2026-03-08 11:09:58 +0800

    Remove duplicate CORS headers from dashboard API files

--- api/dashboard/get_stats.php
+++ api/dashboard/get_stats.php
@@ -2,10 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Content-Type');
 
 require_once '../config/database.php';
 
--- api/dashboard/get_upcoming_birthdays.php
+++ api/dashboard/get_upcoming_birthdays.php
@@ -1,8 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET');
-header('Access-Control-Allow-Headers: Content-Type');
 
 require_once '../config/database.php';
 
