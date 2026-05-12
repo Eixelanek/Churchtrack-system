207cceefa5fb2e379233633b9c3d19fea9de3c22
Clark Ken <clarkken.moron@cvsu.edu.ph>
2026-05-12 09:51:45 +0800

    Fix reset password email link to use Vercel frontend URL

--- api/members/email_verification_utils.php
+++ api/members/email_verification_utils.php
@@ -59,8 +59,8 @@ if (!function_exists('getFrontendBaseUrl')) {
             return rtrim($origin, '/');
         }
 
-        // Default to churchtrack-system.clcc.site for production
-        return 'https://churchtrack-system.clcc.site';
+        // Default to Vercel frontend for production
+        return 'https://churchtrack-system.vercel.app';
     }
 }
 
